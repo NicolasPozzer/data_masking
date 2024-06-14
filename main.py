f883e3c4a1909c087308245cc49334a02c4adb30
NicolasPozzer <pozzernico@gmail.com>
2024-06-14 10:43:20 -0300

    data masking, Done!

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+import pandas as pd
+from faker import Faker
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+name_file = "data-in.csv" # Reads data from a .csv or .txt file.
+
+# Create instance Faker
+fake = Faker()
+
+# Functions Mask data
+def mask_id():
+    return fake.random_number(digits=7, fix_len=True)
+def mask_name():
+    return fake.name()
+
+def mask_last_name():
+    return fake.last_name()
+
+def mask_phone_number():
+    return fake.phone_number()
+
+# Mask Data
+def mask(file):
+    file['ID'] = file['ID'].apply(lambda x: mask_id())
+    file['Name'] = file['Name'].apply(lambda x: mask_name())
+    file['Last Name'] = file['Last Name'].apply(lambda x: mask_last_name())
+    file['Phone'] = file['Phone'].apply(lambda x: mask_phone_number())
+
+def outputData(filepath):
+    print("---------------output data-----------------", "\n", file.head())
+
+    if filepath.endswith(".csv"):
+        file.to_csv('data-out.csv', index=False)
+    else:
+        file.to_csv('data-out.txt', index=False)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# RUN #
+try:
+    file = pd.read_csv(name_file)
+    print("\n","---------------input data-----------------")
+    print(file.head(),"\n")
+    mask(file)
+    outputData(name_file) # save data
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+except Exception as ex:
+    print("Error when trying to read data",ex)
